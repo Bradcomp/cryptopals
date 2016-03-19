c6eadcbf9f304a383fc403a32c91c7412c6c9645
Bradcomp <notpmoc84@hotmail.com>
2016-03-19 00:03:30 -0700

    Set 1 challenge 2

---
+++ set1/fixedxor.py
@@ -0,0 +1,11 @@
+from binascii import b2a_hex
+
+def fixed_xor(s1, s2):
+    b1 = s1.decode('hex')
+    b2 = s2.decode('hex')
+    return "".join([chr(ord(a) ^ ord(b)) for (a, b) in zip(b1, b2)]).encode('hex')
+
+def test():
+    result = fixed_xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')
+    print result
+    assert result == '746865206b696420646f6e277420706c6179'
