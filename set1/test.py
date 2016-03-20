0e69b0d040b95ab6d3342bb93bd065463fc5a5cb
Bradcomp <notpmoc84@hotmail.com>
2016-03-20 13:45:48 -0700

    Added test for part 3

--- set1/test.py
+++ set1/test.py
@@ -1,10 +1,16 @@
 from fixedxor import fixed_xor
+import singlebytexor as sb
 
 def part2():
     result = fixed_xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')
     assert result.encode('hex') == '746865206b696420646f6e277420706c6179'
 
+def part3():
+    plaintext, _ = sb.likely_strings(sb.ciphertext)
+    assert plaintext == "Cooking MC's like a pound of bacon"
+
 def test():
     part2()
+    part3()
 
 test()
